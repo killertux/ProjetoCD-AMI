0a0bc9c277412a3a1ada425ee910e1dc245c1df6
killertux <bruno.canguru@hotmail.com>
2017-04-07 20:16:49 -0300

    Adding the server

---
+++ AMI_Server/build/CMakeFiles/ami_server.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ami_server.dir/main.cpp.o"
+  "ami_server.pdb"
+  "ami_server"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ami_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
