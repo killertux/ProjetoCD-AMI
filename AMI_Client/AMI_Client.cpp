3b5f90a2668d65477f49fccf42991ed36e692a4e
killertux <bruno.canguru@hotmail.com>
2017-04-08 11:00:51 -0300

    Starting to have a working interface

--- AMI_Client/AMI_Client.cpp
+++ AMI_Client/AMI_Client.cpp
@@ -1,22 +1,38 @@
 #include "AMI_Client.h"
 
-#include <QLabel>
-#include <QMenu>
-#include <QMenuBar>
-#include <QAction>
-
 AMI_Client::AMI_Client()
 {
-    QLabel* label = new QLabel( this );
-    label->setText( "Hello World!" );
-    setCentralWidget( label );
-    QAction* action = new QAction(this);
-    action->setText( "Quit" );
-    connect(action, SIGNAL(triggered()), SLOT(close()) );
-    menuBar()->addMenu( "File" )->addAction( action );
+	//So, first let's take care of this boring interface stuff
+	this->mainWindow = new Ui_MainWindow();
+	this->mainWindow->setupUi(this);
+	this->mainWindow->retranslateUi(this);
+	
+	//Can't send text while we are not connected to a server
+	this->mainWindow->lineEditNewMessage->setDisabled(true);
+	this->mainWindow->lineEditNewMessage->setText("Not connected to a server!\n");
+	
+	//Assign signals;
+	this->connect(this->mainWindow->lineEditNewMessage,SIGNAL(returnPressed()),this,SLOT(sendMessage()));
+	this->connect(this->mainWindow->pushButtonConnect,SIGNAL(clicked()),this,SLOT(connectToServer()));
+	this->connect(this->mainWindow->pushButtonDisconnect,SIGNAL(clicked()),this,SLOT(disconnectFromServer()));
+	
 }
 
 AMI_Client::~AMI_Client()
 {}
+
+void AMI_Client::sendMessage(){
+	std::string message = this->mainWindow->lineEditNewMessage->text().toUtf8().constData();
+	std::cout << message << std::endl;
+}
+
+void AMI_Client::connectToServer(){
+}
+
+void AMI_Client::disconnectFromServer(){
+}
+
+
+
 
 #include "AMI_Client.moc"
--- AMI_Client/AMI_Client.h
+++ AMI_Client/AMI_Client.h
@@ -1,15 +1,24 @@
 #ifndef AMI_Client_H
 #define AMI_Client_H
 
+
 #include <QMainWindow>
+#include <string>
+#include <iostream>		//Remeber to remove this
+#include "MainWindows.hpp"
 
 class AMI_Client : public QMainWindow
 {
     Q_OBJECT
+    Ui_MainWindow *mainWindow;
 
 public:
     AMI_Client();
     virtual ~AMI_Client();
+public slots:
+	void sendMessage();
+	void connectToServer();
+	void disconnectFromServer();
 };
 
 #endif // AMI_Client_H
